b2c02fe4cfaf9201a1a41906906b9491e17911af
saadkhan371 <saadkhan371@gmail.com>
2019-01-27 18:26:04 +0500

    new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   pom.xml 	new file:   src/main/java/com/pilotproject/PilotprojectApplication.java 	new file:   src/main/java/com/pilotproject/exception/ResourceNotFoundException.java 	new file:   src/main/java/com/pilotproject/model/Users.java 	new file:   src/main/java/com/pilotproject/repo/UsersRepo.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/pilotproject/pilotproject/PilotprojectApplicationTests.java

---
+++ src/main/java/com/pilotproject/model/Users.java
@@ -0,0 +1,65 @@
+package com.pilotproject.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "Users")
+public class Users implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@NotNull
+	private String name;
+
+	private Date createdAt;
+
+	private Date updatedAt;
+
+}
---
+++ src/test/java/com/pilotproject/pilotproject/PilotprojectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.pilotproject.pilotproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PilotprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
